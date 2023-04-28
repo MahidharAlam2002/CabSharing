3b68b61e67f0a10ac2c971e42990418c3f9e4b55
B.V.S GNANESWAR <cs19btech11007@iith.ac.in>
2023-04-28 15:21:46 +0530

    cleaned codes for dropdown, joindialoguebox, createscheduledialoguebox

--- codeBase/src/Components/succ_login/CreateScheduleDialoguebox.js
+++ codeBase/src/Components/succ_login/CreateScheduleDialoguebox.js
@@ -8,10 +8,6 @@ import axios from'axios';
 
 function CreateScheduleDialoguebox(props) {
     const profileDetails=props.profileDetails;
-    // const row=props.row;
-    // const index=props.index;
-    // const status=props.status;
-    // const Valuelist=props.Valuelist;
     const options=props.options;
     const cancelButton=props.cancelButton;
     const [scheduleDetails, setscheduleDetails] = useState({
@@ -22,53 +18,39 @@ function CreateScheduleDialoguebox(props) {
       });
 	const [errormsg,seterrormsg]=useState('')
     const confirmButton=async()=>{
-		
-        // async function InsertData(){
-			try{
-				if(scheduleDetails.startPlace===null || scheduleDetails.endPlace===null || scheduleDetails.date==='' || scheduleDetails.time==='')
-				{
-					seterrormsg("*All the fields are mandatory");
-					return;
-				}
-				// console.log(scheduleDetails.startPlace[0],scheduleDetails.endPlace[0],scheduleDetails.startPlace[0]===scheduleDetails.endPlace[0]);
-				if(scheduleDetails.startPlace[0]===scheduleDetails.endPlace[0])
-				{
-					seterrormsg("*Start Place and End Place cannot be same ");
-					return;
-				}
-            const res= await axios.get('/createschedule',{params:scheduleDetails});
-            // const res2= await axios.get('/createschedule2',{params:scheduleDetails})
-				// console.log(res);
-				if(res.data.code==='ER_DUP_ENTRY')
-				{
-					seterrormsg('You have already created same schedule');
-					// console.log(res.data.sqlMessage);
-					return;
-				}
-				
-				
-		}
-			catch(err)
+		try
+		{
+			if(scheduleDetails.startPlace===null || scheduleDetails.endPlace===null || scheduleDetails.date==='' || scheduleDetails.time==='')
 			{
-				seterrormsg("fill all details");
-				console.log(err);
+				seterrormsg("*All the fields are mandatory");
 				return;
 			}
-	// 	}
-	// 	try
-    //   {  await InsertData();}
-	//   catch(err){
-	// 	seterrormsg("fill all details");
-	// 	return;
-	//   }
-        await props.reloadButton();
-		
+			if(scheduleDetails.startPlace[0]===scheduleDetails.endPlace[0])
+			{
+				seterrormsg("*Start Place and End Place cannot be same ");
+				return;
+			}
+			const res= await axios.get('/createschedule',{params:scheduleDetails});
+			if(res.data.code==='ER_DUP_ENTRY')
+			{
+				seterrormsg('You have already created same schedule');
+				return;
+			}
+		}
+		catch(err)
+		{
+			seterrormsg("fill all details");
+			console.log(err);
+			return;
+		}
+        await props.reloadButton();	
     }
 
     const handlescheduledetails=(event)=>{
         const { name, value } = event.target;
         setscheduleDetails({ ...scheduleDetails, [name]: value });
     }
+
     useEffect(()=>{
 		seterrormsg('');
 		setscheduleDetails({
@@ -78,163 +60,134 @@ function CreateScheduleDialoguebox(props) {
 			time: ''
 		  });
 	},[props])
+
     return (
         <div className="wrapschedule" data-pos="0">
-		<div className="headbar">
-            <i className="fa-solid fa-handshake"></i> <span style={{marginLeft: '10px', marginRight: '10px'}}> Creating a Schedule  </span><i className="fa-solid fa-handshake"></i> 
-            <i className="closeicon zmdi zmdi-close zmdi-hc-lg" onClick={()=>cancelButton()}></i>
-		</div>
-		<div className="headerschedule">
-			{/* <div className="bg"></div> */}
-			{/* <div className="filter"></div> */}
-			{/* <div className="title"> */}
-				{/* <div className="fromPlace">
-					<span>D</span><span>U</span><span>B</span>
-				</div>
-				<span className="separator"><i className="zmdi zmdi-airplane"></i></span>
-				<div className="toPlace">
-					<span>M</span><span>R</span><span>S</span>
-				</div> */}
-                <div>
-                    <Logo></Logo>
-                </div>
-			{/* </div> */}
-			{/* <div className="map"></div> */}
-		</div>
-        
-        
-
-		<div className="contentschedule">
-			<section>
-				<div className="formschedule">
-                    <div style={{display: 'flex', flexDirection: 'row'}}>
-						<div className="controlschedule select" style={{width: '50%'}} >
-							<div className="controlschedule-head">
-								<i style={{marginRight: '10px'}} className="zmdi zmdi-account-circle"></i>
-								
-								<span className="close"><i className="zmdi zmdi-close"></i></span>
-								<div >
-									{/* <h6>Name : </h6> */}
-									<span className="airport-name" data-role="from">{profileDetails.name} </span>
-								</div>			
-							</div>
-						</div>
-						<div className="controlschedule select" style={{width: '50%'}}>
-							<div className="controlschedule-head">
-								<i style={{marginRight: '10px'}} className="zmdi zmdi-phone"></i>
-								<span className="close"><i className="zmdi zmdi-close"></i></span>
-								<div>
-									{/* <h6>Phone Number</h6> */}
-									<span className="airport-name" data-role="from">{profileDetails.phone}</span>
-								</div>			
-							</div>
-						</div>
+			<div className="headbar">
+				<i className="fa-solid fa-handshake"></i> <span style={{marginLeft: '10px', marginRight: '10px'}}> Creating a Schedule  </span><i className="fa-solid fa-handshake"></i> 
+				<i className="closeicon zmdi zmdi-close zmdi-hc-lg" onClick={()=>cancelButton()}></i>
+			</div>
+			<div className="headerschedule">
+					<div>
+						<Logo></Logo>
 					</div>
-                    <div style={{display: 'flex', flexDirection: 'row'}}>
-                        <div className="controlschedule select" style={{width: '50%'}}>
-                            <div className="controlschedule-head" >
-                                <i className="zmdi zmdi-pin"></i>
-                                <span className="close"><i className="zmdi zmdi-close"></i></span>
-                                <div >
-                                    {/* <h6>From</h6> */}
-                                    <span className="airport-name" data-role="from" >
-                                    <div>
-                                    
-                                    <Dropdown
-                                        isSearchable={true}
-                                        isMulti={false}
-                                        placeHolder="From"
-                                        options={options}
-                                        onChange={(value) =>{ scheduleDetails.startPlace=props.Valuelist(value);}}
-                                        
-                                    />
-                                    </div>
-                                    </span>
-                                </div>			
-                            </div>
-                        </div>
-						<div className="controlschedule select" style={{width: '50%'}}>
-                            <div className="controlschedule-head" >
-                                <i className="zmdi zmdi-pin"></i>
-                                <span className="close"><i className="zmdi zmdi-close"></i></span>
-                                <div >
-                                    <span className="airport-name" data-role="from" >
-                                    <div>
-                                    <Dropdown
-                                        isSearchable={true}
-                                        isMulti={false}
-                                        placeHolder="To"
-                                        options={options}
-                                        onChange={(value) =>{ scheduleDetails.endPlace=props.Valuelist(value);}}
-                                        
-                                    />
-                                    </div>
-                                    </span>
-                                </div>			
-                            </div>
-                        </div>
-                    </div>
-					<div style={{display: 'flex', flexDirection: 'row'}}>
-						<div className="controlschedule dateinput" style={{width: '50%'}}>
-							<div className="controlschedule-head">
-								<i className="zmdi zmdi-calendar"></i>
-								<span className="close"><i className="zmdi zmdi-close"></i></span>
-								<div className="controlschedule-item" style={{display: 'flex', flexDirection: 'row'}}>
-									{/* <h6>Date</h6> */}
-									<span>
-									<input
-										
-										type="date"
-										id="date"
-										name="date"
-										placeholder="Date"
-										value={scheduleDetails.date}
-										onChange={handlescheduledetails}
-										style={{ padding: '10px 16px', borderRadius: 8, outline: 'none', border: 'none', marginRight: 8, width: 200 }}
-										required
-									/>
-									</span>
+			</div>
+			<div className="contentschedule">
+				<section>
+					<div className="formschedule">
+						<div style={{display: 'flex', flexDirection: 'row'}}>
+							<div className="controlschedule select" style={{width: '50%'}} >
+								<div className="controlschedule-head">
+									<i style={{marginRight: '10px'}} className="zmdi zmdi-account-circle"></i>
+									
+									<span className="close"><i className="zmdi zmdi-close"></i></span>
+									<div >
+										<span className="airport-name" data-role="from">{profileDetails.name} </span>
+									</div>			
+								</div>
+							</div>
+							<div className="controlschedule select" style={{width: '50%'}}>
+								<div className="controlschedule-head">
+									<i style={{marginRight: '10px'}} className="zmdi zmdi-phone"></i>
+									<span className="close"><i className="zmdi zmdi-close"></i></span>
+									<div>
+										<span className="airport-name" data-role="from">{profileDetails.phone}</span>
+									</div>			
 								</div>
 							</div>
 						</div>
-						<div className="controlschedule dateinput" style={{width: '50%'}}>
-							<div className="controlschedule-head">
-								<i className="zmdi zmdi-time"></i>
-								<span className="close"><i className="zmdi zmdi-close"></i></span>
-								<div className="controlschedule-item" style={{display: 'flex', flexDirection: 'row'}}>
-									{/* <h6>Time</h6> */}
-									<span>
-									<input
-										
-										type="time"
-										id="time"
-										name="time"
-										placeholder="time"
-										value={scheduleDetails.time}
-										onChange={handlescheduledetails}
-										style={{ padding: '10px 16px', borderRadius: 8, outline: 'none', border: 'none', marginRight: 8, width: 200 }}
-										required
-									/>
-									</span>
+						<div style={{display: 'flex', flexDirection: 'row'}}>
+							<div className="controlschedule select" style={{width: '50%'}}>
+								<div className="controlschedule-head" >
+									<i className="zmdi zmdi-pin"></i>
+									<span className="close"><i className="zmdi zmdi-close"></i></span>
+									<div >
+										<span className="airport-name" data-role="from" >
+										<div>
+											<Dropdown
+												isSearchable={true}
+												isMulti={false}
+												placeHolder="From"
+												options={options}
+												onChange={(value) =>{ scheduleDetails.startPlace=props.Valuelist(value);}}
+												
+											/>
+										</div>
+										</span>
+									</div>			
 								</div>
 							</div>
-							
+							<div className="controlschedule select" style={{width: '50%'}}>
+								<div className="controlschedule-head" >
+									<i className="zmdi zmdi-pin"></i>
+									<span className="close"><i className="zmdi zmdi-close"></i></span>
+									<div >
+										<span className="airport-name" data-role="from" >
+										<div>
+											<Dropdown
+												isSearchable={true}
+												isMulti={false}
+												placeHolder="To"
+												options={options}
+												onChange={(value) =>{ scheduleDetails.endPlace=props.Valuelist(value);}}
+												
+											/>
+										</div>
+										</span>
+									</div>			
+								</div>
+							</div>
+						</div>
+						<div style={{display: 'flex', flexDirection: 'row'}}>
+							<div className="controlschedule dateinput" style={{width: '50%'}}>
+								<div className="controlschedule-head">
+									<i className="zmdi zmdi-calendar"></i>
+									<span className="close"><i className="zmdi zmdi-close"></i></span>
+									<div className="controlschedule-item" style={{display: 'flex', flexDirection: 'row'}}>
+										<span>
+										<input
+											type="date"
+											id="date"
+											name="date"
+											placeholder="Date"
+											value={scheduleDetails.date}
+											onChange={handlescheduledetails}
+											style={{ padding: '10px 16px', borderRadius: 8, outline: 'none', border: 'none', marginRight: 8, width: 200 }}
+											required
+										/>
+										</span>
+									</div>
+								</div>
+							</div>
+							<div className="controlschedule dateinput" style={{width: '50%'}}>
+								<div className="controlschedule-head">
+									<i className="zmdi zmdi-time"></i>
+									<span className="close"><i className="zmdi zmdi-close"></i></span>
+									<div className="controlschedule-item" style={{display: 'flex', flexDirection: 'row'}}>
+										<span>
+										<input
+											type="time"
+											id="time"
+											name="time"
+											placeholder="time"
+											value={scheduleDetails.time}
+											onChange={handlescheduledetails}
+											style={{ padding: '10px 16px', borderRadius: 8, outline: 'none', border: 'none', marginRight: 8, width: 200 }}
+											required
+										/>
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div style={{display:'flex', justifyContent: 'center', color: 'red'}}>{errormsg}</div>
+						<div className="controlschedule">
+							<button type='submit' className="btnSearch" style={{color: '#1C1D21'}} onClick={()=>confirmButton(scheduleDetails)}><strong>Confirm</strong></button>
 						</div>
 					</div>
-					<div style={{display:'flex', justifyContent: 'center', color: 'red'}}>{errormsg}</div>
-					<div className="controlschedule">
-						<button type='submit' className="btnSearch" style={{color: '#1C1D21'}} onClick={()=>confirmButton(scheduleDetails)}><strong>Confirm</strong></button>
-					</div>
-
-				</div>
-				
-				
-				
-
-			</section>
-		</div>		
-	
-	</div>
+				</section>
+			</div>		
+		</div>
     );
 }
 
--- codeBase/src/Components/succ_login/Dropdown.js
+++ codeBase/src/Components/succ_login/Dropdown.js
@@ -1,43 +1,3 @@
-// import React from "react";
-
-// import "./Dropdown.css";
-
-// const Icon = () => {
-//   return (
-//     <svg height="20" width="20" viewBox="0 0 20 20">
-//       <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
-//     </svg>
-//   );
-// };
-
-// const Dropdown = ({ placeHolder, options }) => {
-//   const getDisplay = () => {
-//     return placeHolder;
-//   };
-
-//   return (
-//     <div className="dropdown-container">
-//         <div className="dropdown-input">
-//             <div className="dropdown-selected-value">{getDisplay()}</div>
-//             <div className="dropdown-tools">
-//                 <div className="dropdown-tool">
-//                     <Icon />
-//                 </div>
-//             </div>
-//         </div>    
-//         <div>
-//             {options.map((option)=>(
-//                 <div key={option.value} className="dropdown-item">
-//                     {option.label}
-//                 </div>
-//             ))}
-//         </div>
-//     </div>
-//   );
-// };
-
-// export default Dropdown;
-
 import React, { useEffect, useRef, useState } from "react";
 
 import "./Dropdown.css";
@@ -70,7 +30,7 @@ const Dropdown = ({
   const [searchValue, setSearchValue] = useState("");
   const searchRef = useRef();
   const inputRef = useRef();
-// useEffect(()=>{setSelectedValue(isMulti ? [] : null)},[placeHolder])
+
   useEffect(() => {
     setSearchValue("");
     if (showMenu && searchRef.current) {
@@ -86,42 +46,42 @@ const Dropdown = ({
     };
 
     window.addEventListener("click", handler);
-    return () => {
-      window.removeEventListener("click", handler);
+      return () => {
+        window.removeEventListener("click", handler);
+      };
+    });
+
+    const handleInputClick = (e) => {
+      setShowMenu(!showMenu);
     };
-  });
 
-  const handleInputClick = (e) => {
-    setShowMenu(!showMenu);
-  };
+    const getDisplay = () => {
+      if (!selectedValue || selectedValue.length === 0) {
+        return placeHolder;
+      }
+      if (isMulti) {
+        return (
+          <div className="dropdown-tags">
+            {selectedValue.map((option) => (
+              <div key={option.value} className="dropdown-tag-item">
+                {option.label}
+                <span
+                  onClick={(e) => onTagRemove(e, option)}
+                  className="dropdown-tag-close"
+                >
+                  <CloseIcon />
+                </span>
+              </div>
+            ))}
+          </div>
+        );
+      }
+      return selectedValue.label;
+    };
 
-  const getDisplay = () => {
-    if (!selectedValue || selectedValue.length === 0) {
-      return placeHolder;
-    }
-    if (isMulti) {
-      return (
-        <div className="dropdown-tags">
-          {selectedValue.map((option) => (
-            <div key={option.value} className="dropdown-tag-item">
-              {option.label}
-              <span
-                onClick={(e) => onTagRemove(e, option)}
-                className="dropdown-tag-close"
-              >
-                <CloseIcon />
-              </span>
-            </div>
-          ))}
-        </div>
-      );
-    }
-    return selectedValue.label;
-  };
-
-  const removeOption = (option) => {
-    return selectedValue.filter((o) => o.value !== option.value);
-  };
+    const removeOption = (option) => {
+      return selectedValue.filter((o) => o.value !== option.value);
+    };
 
   const onTagRemove = (e, option) => {
     e.stopPropagation();
